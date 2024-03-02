fa199d11d3038026046bbc659720f6c7d7a76082
YASHUO9 <jp985083@gmail.com>
2024-03-02 11:45:01 +0530

    Readme.md files is updated now

---
+++ Basics of Flask/Flask Setup/app1.py
@@ -0,0 +1,8 @@
+import flask
+#Flask:
+#Flask is a web framework. It is a Python module that lets you develop web applications easily.
+#Flask is a micro-framework. It does not require particular tools or libraries.
+#It has no database abstraction layer, form validation, or any other components where pre-existing third-party libraries provide common functions.
+#However, Flask supports extensions that can add application features as if they were implemented in Flask itself.
+
+print(flask.__version__)
