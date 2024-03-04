45cc32fd24f6640789260939902bca9c1de60864
YASHUO9 <jp985083@gmail.com>
2024-03-04 21:31:11 +0530

    New error has been solved now

--- To do List App/app.py
+++ To do List App/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime as dt
 
 
 #<-----------------------------------To do List App------------------------------------->
@@ -18,15 +18,14 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 
 
-db = SQLAlchemy()
-# initialize the app with the extension
-db.init_app(app)
+db = SQLAlchemy(app)
+
 
 class TodoList(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    time = db.Column(db.DateTime, default=datetime.utcnow)
+    time = db.Column(db.DateTime, default=dt.utcnow)
 
     def __repr__(self):
         return f'{self.sno} - {self.title}'
@@ -37,11 +36,9 @@ class TodoList(db.Model):
 
 @app.route('/')
 def index():
-    todo = TodoList(title='To do List App', desc='This is a simple to do list app')
-    db.session.add(todo) # add the todo to the database
-    db.session.commit() # commit the changes to the database
+    
     allTodo = TodoList.query.all()
-    return render_template('index.html',allTodo=allTodo)
+    return render_template('index.html',alltodo=allTodo)
 
 
 
