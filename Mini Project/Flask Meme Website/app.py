dc3120f71e5b732958ecca1e684b492b219f272b
YASHUO9 <jp985083@gmail.com>
2024-03-09 10:54:40 +0530

    new

--- Mini Project/Flask Meme Website/app.py
+++ Mini Project/Flask Meme Website/app.py
@@ -1,31 +1,27 @@
 from flask import Flask, render_template
 import requests
 
-
 app = Flask(__name__) # creating a Flask app
 
 def get_memes():
     url = "https://meme-api.com/gimme"
-    try:
-        response = requests.get(url)
-        data = response.json()
-        meme_url = data.get('url', None)
-        subreddit = data.get('subreddit', None)
-    except Exception as e:
-        print(f"Error occurred while fetching memes: {e}")
-        meme_url = None
-        subreddit = None
+    
+    # making a GET request to the API   
+    response = requests.get(url)
+    data = response.json()
+    meme_url = data.get('url')
+    subreddit = data.get('subreddit')
+    
     return meme_url, subreddit
-
-
 
 @app.route('/')
 def index():
     """ Function to render the index.html template"""
     
     meme, subreddit = get_memes() # unpacking the tuple
+    print("Meme:", meme)
+    print("Subreddit:", subreddit)
     return render_template('index.html', meme=meme, subreddit=subreddit)
-
 
 if __name__ == '__main__':
     # running the app
