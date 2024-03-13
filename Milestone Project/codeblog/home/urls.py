57682daaa7ef3cca93412485d4ff23ed8f20d15b
YASHUO9 <jp985083@gmail.com>
2024-03-13 11:59:11 +0530

    new files added now

--- Milestone Project/codeblog/home/urls.py
+++ Milestone Project/codeblog/home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path('contact', views.contact, name="contact" ),
        path('about', views.about, name="about" ),
+       path('home', views.home, name="home" ),
     
 
 ]
